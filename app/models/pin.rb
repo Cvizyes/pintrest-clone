fbe56b24b4c01093c5cf4c44985d9ba8aff10908
Gabriel <carlosvizcaino718@gmail.com>
2017-12-19 11:38:52 -0400

    add configuration upvotes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,7 @@
 class Pin < ApplicationRecord
-	belongs_to :user
+  acts_as_votable
+  belongs_to :user
 
-	has_attached_file :image, styles: {medium: "300*300>"}
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: {medium: "300*300>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
